f970d2e64a9e88f2035ecbf3041c1bc1401cfdda
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-01-04 14:12:27 +0100

    Création branche dev Création fichier galerie.js Appel API categories et works avec fetch

---
+++ FrontEnd/galerie.js
@@ -0,0 +1,4 @@
+//Récupération des catégories en appelant l' API categories
+fetch("http://localhost:5678/api/categories")
+//Récupération des projets en appelant l' API works
+fetch("http://localhost:5678/api/works")
