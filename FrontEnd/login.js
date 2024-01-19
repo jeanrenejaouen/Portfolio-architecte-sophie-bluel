8c52b7dd71c829c30a1310ea3a19074e9ac8c212
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-01-19 13:44:42 +0100

    Travaux sur login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,48 +1,86 @@
 /* import { tokenWrapper } from "./api.js" */
+document.addEventListener("DOMLoaded", function(){
+    const loginLink = document.querySelector("#nav-login");
+
+    //Vérifier si le token est présent dans le localStorage
+    const token = sessionStorage.getItem("token");
+    console.log(token);
+    if(token){
+        //Changer le texte en "logout" si le token est présent
+        loginLink.textContent = "logout";
+    };
+});
 
 // Récupération des éléments HTML
 
 const submitBtn = document.querySelector(".log-btn")
 
-
-/* const loginError = document.querySelector(".login-error") */
+const loginError = document.querySelector(".login-error")
 
 let emailInput = ""
 let passwordInput = ""
 
-/**
- * Événement de saisie pour l'input de l'email.
- * @param {Event} e - L'événement de saisie.
- */
-email.addEventListener("input", (e) => {
-    emailInput = e.target.value
-})
 
-/**
- * Événement de saisie pour l'input du mot de passe.
- * @param {Event} e - L'événement de saisie.
- */
-password.addEventListener("input", (e) => {
-    passwordInput = e.target.value
-})
 
 /**
  * Événement de clic sur le bouton de soumission du formulaire de connexion.
  * @param {Event} e - L'événement de clic.
  */
 submitBtn.addEventListener("click", async (e) => {
+    // Désactivation du comportement par défaut du navigateur
     e.preventDefault()
-    const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
-    let user = { "email": email, "password": password };
-    console.log(user);
-    const responseForLogin = await tokenWrapper(user)
+    // Création de l'objet user avec email et password
     
-    if (!responseForLogin.ok || !emailInput || !passwordInput) {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    let user = { "email": email, "password": password };
+    
+    console.log(user);   
+
+    //Création de la charge utile au format JSON
+    const chargeUtile = JSON.stringify(user);
+
+    //Appel de la fonction fetch avec ses arguments
+        const reponse = await fetch ("http://localhost:5678/api/users/login",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: chargeUtile,
+        
+
+        /* const responseForLogin = await tokenWrapper(user) */ 
+        /* const responseForLogin = await reponse.json();
+   
+        console.log(responseForLogin); */
+
+     /* if (!responseForLogin.ok || !emailInput || !passwordInput) {
         loginError.innerHTML = "Veuillez entrer un email et un mot de passe valide";
     } else {
         let userOnline = await responseForLogin.json()
         sessionStorage.setItem("userOnline", JSON.stringify(userOnline))
-        window.location.href = "/index.html";
-    }
-})
+        window.location.href = "/index.html"; */
+    
+ 
+
+
+    }); 
+    
+
+     if ( email === 'ja@ja.fr' && password === '9999' ) { 
+   
+    // Autoriser l'accès à la page restreinte
+        window.location.href = 'index.html'; 
+            
+     } 
+     
+     else { 
+    // Afficher un message d'erreur si l'email ou le mot de passe est incorrect
+    document.getElementById('login-error').textContent = 'Erreur dans l’identifiant ou le mot de passe';
+    document.getElementById('login-error').style.color = "red"; 
+    
+  }
+
+});
+
+
+/* window.localStorage.setItem("userId", "token"); */
+
